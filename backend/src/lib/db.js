9e87feed83e8d052759b24bf64bdea42884cfeae
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2025-11-03 22:55:36 +0530

    safety check added for DB_URL

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,10 @@ import ENV from "./env.js";
 
 export async function connectToDB() {
   try {
+    if (!ENV.DB_URL) {
+      throw new Error("'DB_URL' is not defined in environment variables!");
+    }
+
     const mongooseInstance = await mongoose.connect(ENV.DB_URL);
     console.log("Connected to MongoDB:", mongooseInstance.connection.host);
 
