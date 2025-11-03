924f6f209824f40be7ee9978f7986e3bf9f810a5
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2025-11-03 22:31:24 +0530

    mongoDB, third party providers, and env vars setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import ENV from "./env.js";
+
+export async function connectToDB() {
+  try {
+    const mongooseInstance = await mongoose.connect(ENV.DB_URL);
+    console.log("Connected to MongoDB:", mongooseInstance.connection.host);
+
+    mongoose.connection.on("disconnected", () => {
+      console.warn("Mongoose disconnected!");
+    });
+    mongoose.connection.on("error", (err) => {
+      console.error("Mongoose connection error:", err);
+    });
+
+    return mongooseInstance;
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+    process.exit(1);
+  }
+}
--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -2,6 +2,11 @@ const ENV = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT ?? 3000,
   DB_URL: process.env.DB_URL ?? "",
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY ?? "",
+  INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY ?? "",
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY ?? "",
+  STREAM_ACCESS_KEY: process.env.STREAM_ACCESS_KEY ?? "",
+  STREAM_ACCESS_SECRET: process.env.STREAM_ACCESS_SECRET ?? "",
 };
 
 export default ENV;
