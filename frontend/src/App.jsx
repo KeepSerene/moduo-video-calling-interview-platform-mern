665961e162ba364845377ce3d37d61c64443ea90
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2025-11-05 15:18:16 +0530

    minor issues fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProblemsPage from "./pages/ProblemsPage";
-import Toaster from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+if (!import.meta.env.VITE_SERVER_BASE_URL) {
+  throw new Error("Server base URL is missing!");
+}
+
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_BASE_URL,
   // instructs the browser to include credentials such as cookies, authentication headers, TLS client certificates
