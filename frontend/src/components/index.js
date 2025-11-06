b97bc14f9e04482a83493da9c14c1b09886597e6
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2025-11-06 19:23:54 +0530

    implemented problems page

---
+++ frontend/src/components/index.js
@@ -0,0 +1,1 @@
+export { default as ProtectedRouteNavbar } from "./ProtectedRouteNavbar";
---
+++ frontend/src/lib/utils.js
@@ -0,0 +1,12 @@
+export function getDifficultyBadgeClassName(difficulty) {
+  switch (difficulty.toLowerCase()) {
+    case "easy":
+      return "badge-success";
+    case "medium":
+      return "badge-warning";
+    case "hard":
+      return "badge-error";
+    default:
+      return "badge-ghost";
+  }
+}
